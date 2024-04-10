ab74740da5099bf899414396af67ca1571f2caf0
arashagp <arash.qardashpoor@gmail.com>
2024-04-10 22:01:02 +0330

    create Supper Offer

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,6 @@
 let slideIndex = 1;
+let remainingTime = 70000;
+
 const setSlide = (input , index) => {
     slideIndex = index;
     let item = document.querySelector(`#${input}`);
@@ -18,3 +20,21 @@ setInterval(() => {
 
     setSlide(`slide${slideIndex}` , slideIndex)
 }, 4000);
+
+let setTime = () => {
+    
+    if(remainingTime == 0) return;
+
+    let hours = Math.floor(remainingTime / 3600);
+    let minute = Math.floor((remainingTime % 3600) / 60);
+    let second = (remainingTime % 3600) % 60;
+
+    document.querySelector('#hours').innerHTML = hours;
+    document.querySelector('#minutes').innerHTML = minute;
+    document.querySelector('#seconds').innerHTML = second;
+}
+
+setInterval(() => {
+    remainingTime -= 1;
+    setTime();
+}, 1000);
